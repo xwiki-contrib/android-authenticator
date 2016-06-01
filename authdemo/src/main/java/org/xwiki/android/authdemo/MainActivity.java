9189aedb162c1c964cbb9d1655072d2c17f87985
fitzlee <fitz.lee@outlook.com>
2016-06-01 22:02:45 +0800

    update authdemo, adding signature

--- authdemo/src/main/java/org/xwiki/android/authdemo/MainActivity.java
+++ authdemo/src/main/java/org/xwiki/android/authdemo/MainActivity.java
@@ -14,6 +14,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -41,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         mAccountManager = AccountManager.get(this);
+
+        ((TextView) findViewById(R.id.signature_textview)).setText("Signature="+SystemTools.getSign(this, getPackageName())+"\nPackageName="+getPackageName());
 
         findViewById(R.id.btnAddAccount).setOnClickListener(new View.OnClickListener() {
             @Override
