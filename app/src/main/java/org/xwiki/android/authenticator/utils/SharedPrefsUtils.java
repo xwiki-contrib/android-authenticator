632b94d85b0c41a0730ce3b59a129c86f9aa5da7
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-05-03 09:23:22 +0800

    fix SharedPrefs commits calls

--- app/src/main/java/org/xwiki/android/authenticator/utils/SharedPrefsUtils.java
+++ app/src/main/java/org/xwiki/android/authenticator/utils/SharedPrefsUtils.java
@@ -37,64 +37,59 @@ public class SharedPrefsUtils {
     public static void putValue(Context context, String key, int value) {
         Editor sp = context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();
         sp.putInt(key, value);
-        sp.commit();
+        sp.apply();
     }
 
     public static void putValue(Context context, String key, boolean value) {
         Editor sp = context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();
         sp.putBoolean(key, value);
-        sp.commit();
+        sp.apply();
     }
 
     public static void putValue(Context context, String key, String value) {
         Editor sp = context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();
         sp.putString(key, value);
-        sp.commit();
+        sp.apply();
     }
 
     public static int getValue(Context context, String key, int defValue) {
         SharedPreferences sp = context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);
-        int value = sp.getInt(key, defValue);
-        return value;
+        return sp.getInt(key, defValue);
     }
 
     public static boolean getValue(Context context, String key, boolean defValue) {
         SharedPreferences sp = context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);
-        boolean value = sp.getBoolean(key, defValue);
-        return value;
+        return sp.getBoolean(key, defValue);
     }
 
     public static String getValue(Context context, String key, String defValue) {
         SharedPreferences sp = context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);
-        String value = sp.getString(key, defValue);
-        return value;
+        return sp.getString(key, defValue);
     }
 
     public static void putArrayList(Context context, String key, List<String> list) {
         SharedPreferences sp = context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);
         Editor edit = sp.edit();
-        Set<String> set = new HashSet<String>();
-        set.addAll(list);
+        Set<String> set = new HashSet<>(list);
         edit.putStringSet(key, set);
-        edit.commit();
+        edit.apply();
     }
 
     public static List<String> getArrayList(Context context, String key) {
         SharedPreferences sp = context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);
         Set<String> set = sp.getStringSet(key, null);
         if (set == null) return null;
-        ArrayList<String> list = new ArrayList<>(set);
-        return list;
+        return new ArrayList<>(set);
     }
 
     public static void clearAll(Context context) {
         SharedPreferences sp = context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);
-        sp.edit().clear().commit();
+        sp.edit().clear().apply();
     }
 
     public static void removeKeyValue(Context context, String key) {
         SharedPreferences sp = context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);
-        sp.edit().remove(key).commit();
+        sp.edit().remove(key).apply();
     }
 
 }
