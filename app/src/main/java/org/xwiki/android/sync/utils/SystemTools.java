b162b3749e7af93936f33844a22bf36be015a7c5
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-06-21 16:46:09 +0800

    ANDAUTH-41 Fill and fix documentation * Docs and refactor for SystemTools

--- app/src/main/java/org/xwiki/android/sync/utils/SystemTools.java
+++ app/src/main/java/org/xwiki/android/sync/utils/SystemTools.java
@@ -19,275 +19,33 @@
  */
 package org.xwiki.android.sync.utils;
 
-import android.app.ActivityManager;
-import android.app.ActivityManager.MemoryInfo;
-import android.app.ActivityManager.RunningAppProcessInfo;
-import android.app.ActivityManager.RunningServiceInfo;
-import android.app.Application;
-import android.app.KeyguardManager;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.Uri;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.os.Build;
-import android.telephony.TelephonyManager;
-import android.util.Log;
-
-import org.xwiki.android.sync.exceptions.ReadPhoneStateException;
-
-import java.io.File;
-import java.security.MessageDigest;
-import java.util.List;
 
 /**
- * system tools
+ * System tools.
+ *
+ * @version $Id$
  */
 public final class SystemTools {
-    private static final String TAG = "SystemTools";
 
     /**
-     * Get IMEI
-     * @throws ReadPhoneStateException - will be thrown in cases when need to get permission to read
-     *                                  phone state
-     */
-    public static String getPhoneIMEI(Context cxt) throws ReadPhoneStateException{
-        TelephonyManager tm = (TelephonyManager) cxt
-                .getSystemService(Context.TELEPHONY_SERVICE);
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                return tm.getImei();
-            } else {
-                return tm.getMeid();
-            }
-        } catch (SecurityException e) {
-            throw new ReadPhoneStateException(e);
-        }
-    }
-
-    /**
-     * return System version
+     * Get from package version name
      *
-     * @return like 2.3.3
-     */
-    public static String getSystemVersion() {
-        return android.os.Build.VERSION.RELEASE;
-    }
-
-
-    /**
-     * check whether network is connected
-     */
-    public static boolean checkNet(Context context) {
-        ConnectivityManager cm = (ConnectivityManager) context
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo info = cm.getActiveNetworkInfo();
-        if (info != null && info.isAvailable()) return true;
-        return false;
-    }
-
-    public static boolean checkWifi(Application context) {
-        WifiManager mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        WifiInfo wifiInfo = mWifiManager.getConnectionInfo();
-        int ipAddress = wifiInfo == null ? 0 : wifiInfo.getIpAddress();
-        return mWifiManager.isWifiEnabled() && ipAddress != 0;
-    }
-
-
-    /**
-     * foreground or background
-     */
-    public static boolean isBackground(Context context) {
-        ActivityManager activityManager = (ActivityManager) context
-                .getSystemService(Context.ACTIVITY_SERVICE);
-        List<RunningAppProcessInfo> appProcesses = activityManager
-                .getRunningAppProcesses();
-        for (RunningAppProcessInfo appProcess : appProcesses) {
-            if (appProcess.processName.equals(context.getPackageName())) {
-                if (appProcess.importance == RunningAppProcessInfo.IMPORTANCE_BACKGROUND) {
-                    // background
-                    return true;
-                } else {
-                    // foreground
-                    return false;
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * whether phone is sleeping
-     */
-    public static boolean isSleeping(Context context) {
-        KeyguardManager kgMgr = (KeyguardManager) context
-                .getSystemService(Context.KEYGUARD_SERVICE);
-        return kgMgr.inKeyguardRestrictedInputMode();
-    }
-
-    /**
-     * apk install
-     *
-     * @param context
-     * @param file
-     */
-    public static void installApk(Context context, File file) {
-        Intent intent = new Intent();
-        intent.setAction("android.intent.action.VIEW");
-        intent.addCategory("android.intent.category.DEFAULT");
-        intent.setType("application/vnd.android.package-archive");
-        intent.setData(Uri.fromFile(file));
-        intent.setDataAndType(Uri.fromFile(file),
-                "application/vnd.android.package-archive");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
-    }
-
-    /**
-     * get app version name
+     * @param context Context to get version name
+     * @return version as how it was present in user settings
      */
     public static String getAppVersionName(Context context) {
         try {
-            return context.getPackageManager().getPackageInfo(
-                    context.getPackageName(), 0).versionName;
+            return context
+                .getPackageManager()
+                .getPackageInfo(
+                    context.getPackageName(),
+                    0
+                )
+                .versionName;
         } catch (NameNotFoundException e) {
-            throw new RuntimeException(SystemTools.class.getName()
-                    + "the application not found");
+            throw new RuntimeException(SystemTools.class.getName() + "the application not found");
         }
-    }
-
-    /**
-     * get app version code
-     */
-    public static int getAppVersionCode(Context context) {
-        try {
-            return context.getPackageManager().getPackageInfo(
-                    context.getPackageName(), 0).versionCode;
-        } catch (NameNotFoundException e) {
-            throw new RuntimeException(SystemTools.class.getName()
-                    + "the application not found");
-        }
-    }
-
-    /**
-     * return Home，run in background
-     */
-    public static void goHome(Context context) {
-        Intent mHomeIntent = new Intent(Intent.ACTION_MAIN);
-        mHomeIntent.addCategory(Intent.CATEGORY_HOME);
-        mHomeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-        context.startActivity(mHomeIntent);
-    }
-
-    /**
-     * get application signature
-     *
-     * @param context
-     * @param pkgName
-     */
-    public static String getSign(Context context, String pkgName) {
-        try {
-            PackageInfo pis = context.getPackageManager().getPackageInfo(
-                    pkgName, PackageManager.GET_SIGNATURES);
-            return hexdigest(pis.signatures[0].toByteArray());
-        } catch (NameNotFoundException e) {
-            throw new RuntimeException(SystemTools.class.getName() + "the "
-                    + pkgName + "'s application not found");
-        }
-    }
-
-    /**
-     * transfer signature to 32bit
-     */
-    private static String hexdigest(byte[] paramArrayOfByte) {
-        final char[] hexDigits = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97,
-                98, 99, 100, 101, 102};
-        try {
-            MessageDigest localMessageDigest = MessageDigest.getInstance("MD5");
-            localMessageDigest.update(paramArrayOfByte);
-            byte[] arrayOfByte = localMessageDigest.digest();
-            char[] arrayOfChar = new char[32];
-            for (int i = 0, j = 0; ; i++, j++) {
-                if (i >= 16) {
-                    return new String(arrayOfChar);
-                }
-                int k = arrayOfByte[i];
-                arrayOfChar[j] = hexDigits[(0xF & k >>> 4)];
-                arrayOfChar[++j] = hexDigits[(k & 0xF)];
-            }
-        } catch (Exception e) {
-        }
-        return "";
-    }
-
-    /**
-     * available memory size
-     *
-     * @param cxt context
-     * @return memory size
-     */
-    public static int getDeviceUsableMemory(Context cxt) {
-        ActivityManager am = (ActivityManager) cxt
-                .getSystemService(Context.ACTIVITY_SERVICE);
-        MemoryInfo mi = new MemoryInfo();
-        am.getMemoryInfo(mi);
-        return (int) (mi.availMem / (1024 * 1024));
-    }
-
-    /**
-     * clean services and background
-     *
-     * @param cxt context
-     * @return clean app count
-     */
-    public static int gc(Context cxt) {
-        long i = getDeviceUsableMemory(cxt);
-        int count = 0; // clean app count
-        ActivityManager am = (ActivityManager) cxt
-                .getSystemService(Context.ACTIVITY_SERVICE);
-        // get services which are running.
-        List<RunningServiceInfo> serviceList = am.getRunningServices(100);
-        if (serviceList != null)
-            for (RunningServiceInfo service : serviceList) {
-                if (service.pid == android.os.Process.myPid())
-                    continue;
-                try {
-                    android.os.Process.killProcess(service.pid);
-                    count++;
-                } catch (Exception e) {
-                    e.getStackTrace();
-                    continue;
-                }
-            }
-
-        // get processes which are running
-        List<RunningAppProcessInfo> processList = am.getRunningAppProcesses();
-        if (processList != null)
-            for (RunningAppProcessInfo process : processList) {
-                // >RunningAppProcessInfo.IMPORTANCE_SERVICE  -> NULL process
-                // >RunningAppProcessInfo.IMPORTANCE_VISIBLE -> background process
-                if (process.importance > RunningAppProcessInfo.IMPORTANCE_VISIBLE) {
-                    // pkgList get packages in this process
-                    String[] pkgList = process.pkgList;
-                    for (String pkgName : pkgList) {
-                        Log.d(TAG, "======killing：" + pkgName);
-                        try {
-                            am.killBackgroundProcesses(pkgName);
-                            count++;
-                        } catch (Exception e) {
-                            e.getStackTrace();
-                            continue;
-                        }
-                    }
-                }
-            }
-        Log.d(TAG, "Clean" + (getDeviceUsableMemory(cxt) - i) + "M memory!");
-        return count;
     }
 }
