6795a9a2215bd03275c98e16115878c622afd37f
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-06-21 10:11:47 +0800

    ANDAUTH-41 Fill and fix documentation * Docs and refactor for BaseApiManager

--- app/src/main/java/org/xwiki/android/sync/rest/BaseApiManager.java
+++ app/src/main/java/org/xwiki/android/sync/rest/BaseApiManager.java
@@ -20,6 +20,7 @@
 package org.xwiki.android.sync.rest;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import org.xwiki.android.sync.Constants;
 import org.xwiki.android.sync.utils.SharedPrefsUtils;
@@ -29,12 +30,38 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * Will help to contain and separate functionality of working with API
+ *
+ * @version $Id$
+ */
 public class BaseApiManager {
 
-    private final Retrofit retrofit;
+    /**
+     * Main services which provide work with auth, getting groups/users and other
+     */
     private final XWikiServices xWikiServices;
+
+    /**
+     * Helper which work with downloading and managing of photos
+     *
+     * @since 0.4
+     */
     private final XWikiPhotosManager xWikiPhotosManager;
 
+    /**
+     * Base constructor which create {@link OkHttpClient} with {@link XWikiInterceptor} which will
+     * be used in {@link Retrofit} for correct handling of requests.
+     *
+     * @param baseUrl Url which will be used as base for all requests in this manager. Can be:
+     *                <ul>
+     *                  <li>http://www.xwiki.org/xwiki/</li>
+     *                  <li>http://some.site.url/xwiki/</li>
+     *                  <li>http://123.231.213.132/xwiki/</li>
+     *                  <li>http://123.231.213.132:123456/xwiki/</li>
+     *                </ul>
+     *                Strongly recommended to end url with <b>/</b> symbol, but not necessary
+     */
     public BaseApiManager(String baseUrl) {
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
@@ -46,7 +73,7 @@ public class BaseApiManager {
             baseUrl = baseUrl + "/";
         }
 
-        retrofit = new Retrofit.Builder()
+        final Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
@@ -56,28 +83,61 @@ public class BaseApiManager {
         xWikiServices = initXWikiServices(retrofit);
         xWikiPhotosManager = initXWikiPhotosManager(okHttpClient, baseUrl);
     }
-    
+
+    /**
+     * Additional constructor which will try to get base url from {@link SharedPrefsUtils} using
+     * context and {@link Constants#SERVER_ADDRESS} preference name
+     *
+     * @param context Will be used to get info from shared preferences
+     */
     public BaseApiManager(Context context) {
         this(SharedPrefsUtils.getValue(context, Constants.SERVER_ADDRESS, null));
     }
-    
+
+    /**
+     * Create {@link XWikiServices} using given {@link Retrofit} instance
+     *
+     * @param retrofit Context for creation object
+     * @return Created object
+     * @see Retrofit#create(Class)
+     *
+     * @since 0.4
+     */
     private static XWikiServices initXWikiServices(
-            Retrofit retrofit
+        @NonNull Retrofit retrofit
     ) {
         return retrofit.create(XWikiServices.class);
     }
 
+    /**
+     * Create {@link XWikiPhotosManager} using given {@link OkHttpClient} client and base url
+     *
+     * @param client Will be used as one point of requests for result instance
+     * @param baseUrl Will be used as root for all requests
+     * @return Instance
+     * @see XWikiPhotosManager#XWikiPhotosManager(OkHttpClient, String)
+     *
+     * @since 0.4
+     */
     private static XWikiPhotosManager initXWikiPhotosManager(
-            OkHttpClient client,
-            String baseUrl
+        @NonNull OkHttpClient client,
+        @NonNull String baseUrl
     ) {
         return new XWikiPhotosManager(client, baseUrl);
     }
 
+    /**
+     * @return {@link #xWikiServices}
+     */
     public XWikiServices getXwikiServicesApi() {
         return xWikiServices;
     }
 
+    /**
+     * @return {@link #xWikiPhotosManager}
+     *
+     * @since 0.4
+     */
     public XWikiPhotosManager getXWikiPhotosManager() {
         return xWikiPhotosManager;
     }
