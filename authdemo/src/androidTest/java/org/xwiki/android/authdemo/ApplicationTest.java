b1b22af71e24e165f9c060ff28063cd3ed2cd0a6
jaindiv26 <divyanshjain36@gmail.com>
2019-06-21 15:56:37 +0530

    Changes made in gradel files to download and unzip xwiki local instance files.

--- authdemo/src/androidTest/java/org/xwiki/android/authdemo/ApplicationTest.java
+++ authdemo/src/androidTest/java/org/xwiki/android/authdemo/ApplicationTest.java
@@ -1,13 +1,10 @@
 package org.xwiki.android.authdemo;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
-    }
+public class ApplicationTest  {
+
 }
